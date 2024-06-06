fb4d9409be27046b79d441df1c602e8641e6df35
Riccialdelli Emiliano <ericcialdelli@arba.gov.ar>
2024-06-06 12:11:21 -0300

    Commit en Rama2 1.3

--- PruebaGIT/src/prueba/Prueba2.java
+++ PruebaGIT/src/prueba/Prueba2.java
@@ -16,5 +16,9 @@ public class Prueba2 {
 		
 		System.out.println("Este metodo lo defini en la rama2");
 	}	
-	
+
+	public void met6(){
+		
+		System.out.println("Este metodo lo defini en la rama2");
+	}	
 }
