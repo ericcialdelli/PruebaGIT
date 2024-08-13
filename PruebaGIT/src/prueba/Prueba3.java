e611a3ba6d38d1a3f4ee4dda6a75e7518892bcb9
Riccialdelli Emiliano <ericcialdelli@arba.gov.ar>
2024-08-13 15:11:22 -0300

    rama 1

--- PruebaGIT/src/prueba/Prueba3.java
+++ PruebaGIT/src/prueba/Prueba3.java
@@ -10,7 +10,12 @@ public class Prueba3 {
 		
 		System.out.println("Estoy en Rama2");
 	}
+
+	public void met8(){
 		
+		System.out.println("Estoy en Rama1");
+	}	
+	
 	public static void main(String[] args) {
 	
 		List<Integer> lista = Arrays.asList (1,2,3,4,5,6,7,8,9,10);
